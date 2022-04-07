0aaeb89eb9cd0fdcc833eac4bacf1d3f6261f7bb
choikihun <rlgns923@naver.com>
2022-04-07 18:52:46 +0900

    Initial Commit

---
+++ RealmPratice3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmPratice3
+//
+//  Created by 최기훈 on 2022/04/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
